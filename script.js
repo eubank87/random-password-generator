25f03907d40ff12dd871e0dabe3c240bb12dca7a
eubank87 <eubank.ashley@gmail.com>
2020-12-09 06:06:28 -0800

    changed yes/no prompts to confirms

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // When generate pw button is clicked:
 
-// TODO: ask user how many characters do they need in their pw (8-128)
+// ask user how many characters do they need in their pw (8-128)
 var passwordLength = prompt("How many characters does your password need?");
 
 // TODO: store user input for total characters
 
 // TODO: ask user if they want to include a number
-var numbers = prompt("Does your password need a number?");
+var numbers = confirm("Does your password need a number?");
 
 // TODO: store user input for numbers
 
 // TODO: ask user if they want to include special characters
-var specialChar = prompt("Does your password need a special character?");
+var specialChar = confirm("Does your password need a special character?");
 
 // TODO: store user input for special characters
 
 // TODO: ask user if they want to inclue lower case
-var lowerCase = prompt("Do you want your password to include lower case letters?");
+var lowerCase = confirm("Do you want your password to include lower case letters?");
 
 // TODO: store user infor for lower case
 
 // TODO: ask user if they want upper case
-var upperCase = prompt("Do you want your password to include upper case letters?");
+var upperCase = confirm("Do you want your password to include upper case letters?");
 
 // TODO: store user info for upper case 
 
